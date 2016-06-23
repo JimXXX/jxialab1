67a98281770896750bfc43cf3b4c052ccdc4976b
Jimxia <xiajxsh@gmail.com>
2016-06-22 21:18:54 -0700

    Initial commit

---
+++ src/jxiaLab1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Jingxuan Xia on 6/22/16.
+ */
+
+public class jxiaLab1 {
+    public static void main(String[] args){
+
+        String name = "Jingxuan Xia";
+        String experience = "I have programmed before";
+        int currentYear = 2016;
+        int YearofBirth = 1990;
+        int myage = currentYear - YearofBirth;
+
+        System.out.println("Hi my name is " + name);
+        System.out.println("I was born in " + YearofBirth + ", and I am " + myage + " years old.");
+        System.out.println(experience);
+
+    }
+}
